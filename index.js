2ea936909a6717eb8cc3fcd1991adedff64493cf
MarentDev <marentdev.pro@gmail.com>
2020-05-28 13:06:35 +0200

    Async/Await convertion && Add defaultExchange options && update mechanism

--- index.js
+++ index.js
@@ -1,68 +1,129 @@
-var dns = require('dns'),
-    net = require('net'),
-    os = require('os');
+const dns = require("dns");
+const net = require("net");
 
-module.exports = function (email, callback, timeout, from_email) {
-	timeout = timeout || 5000;
-	from_email = from_email || email;
-	if (!/^\S+@\S+$/.test(email)) {
-		callback(false,'Bad mail format');
-		return;
-	}
-	dns.resolveMx(email.split('@')[1], function(err, addresses){
+function checkEmail() {
+	var email = null;
+	var options = {};
+	var callback = null;
+
+	if (typeof arguments[0] === "string")
+		email = arguments[0];
+
+	if (typeof arguments[1] === "function")
+		callback = arguments[1];
+	else if (typeof arguments[1] === "object")
+		options = arguments[1];
+
+	if (!callback && typeof arguments[2] === "function")
+		callback = arguments[2];
+
+	var timeout = options.timeout || 5000;
+	var fromEmail = options.fromEmail || email;
+	var defaultExchange = options.defaultExchange ? options.defaultExchange : null;
+
+	if (typeof callback !== "function")
+		return callback(false, "Callback is not a function.");
+
+	if (!(/^\S+@\S+$/).test(email))
+		return callback(false, "Bad email format.");
+
+	dns.resolveMx(email.split("@")[1], function (err, addresses) {
 		if (err || addresses.length === 0) {
-			callback(false,err);
+			callback(false, err);
 			return;
 		}
+
 		var conn = net.createConnection(25, addresses[0].exchange);
-		var commands = [ "helo " + addresses[0].exchange, "mail from: <"+from_email+">", "rcpt to: <"+email+">" ];
+		var commands = [
+			`helo ${defaultExchange ? defaultExchange : addresses[0].exchange}`,
+			`mail from: <${fromEmail}>`,
+			`rcpt to: <${email}>`
+		];
 		var i = 0;
-		conn.setEncoding('ascii');
+
+		conn.setEncoding("ascii");
 		conn.setTimeout(timeout);
 
-		conn.on('error', function(err) {
-			conn.emit('false',err);
+		conn.on("error", (err) => {
+			conn.emit("false", err);
 		});
-		conn.on('false', function (data) {
-			callback(false,data);
+
+		conn.on("false", (data) => {
 			conn.end();
+			return callback(false, data);
 		});
-		conn.on('connect', function() {
-			conn.on('prompt', function (data) {
-				if(i < 3){
+
+		conn.on("connect", () => {
+			conn.on("prompt", (data) => {
+				if (i < 3) {
 					conn.write(commands[i]);
-					conn.write('\r\n');
+					conn.write("\r\n");
 					i++;
-				} else {
-					callback(true,data);
+				}
+				else {
 					conn.end();
 					conn.destroy(); //destroy socket manually
+					return callback(true, data);
 				}
 			});
-			conn.on('undetermined', function (data) {
+
+			conn.on("undetermined", (data) => {
 				//in case of an unrecognisable response tell the callback we're not sure
-				callback(false, data);
 				conn.end();
 				conn.destroy(); //destroy socket manually
+				return callback(false, data);
 			});
-			conn.on('timeout', function () {
-				// conn.emit('undetermined');
-                callback(false, 'Timeout');
-                conn.end();
-                conn.destroy();
+
+			conn.on("Timeout", () => {
+				conn.end();
+				conn.destroy();
+				return callback(false, "Timeout");
 			});
-            conn.on('data', function(data) {
-                if(data.indexOf("220") == 0 || data.indexOf("250") == 0 || data.indexOf("\n220") != -1 || data.indexOf("\n250") != -1) {
-                    conn.emit('prompt',data);
-                } else if(data.indexOf("\n550") != -1 || data.indexOf("550") == 0) {
-                    conn.emit('false',data);
-                } else {
-                    conn.emit('undetermined',data);
-                }
-            });
+
+			conn.on("data", (data) => {
+				if (data.indexOf("220") === 0 || data.indexOf("250") === 0 || data.indexOf("\n220") !== -1 || data.indexOf("\n250") !== -1) {
+					conn.emit("prompt", data);
+				}
+				else if (data.indexOf("\n550") !== -1 || data.indexOf("550") === 0) {
+					conn.emit("false", data);
+				}
+				else {
+					conn.emit("undetermined", data);
+				}
+			});
 		});
 	});
-};
+}
 
-// compatibility
-module.exports.check = module.exports;
+/**
+ * @typedef Options
+ * @type {object}
+ * @property {string} [timeout] - Milliseconds before timeout.
+ * @property {string} [fromEmail] - Email to use for the test.
+ * @property {number} [defaultExchange] - Default exchange mx to test email with (ex: mx.google.com).
+ */
+
+/**
+ * Test if an email is valid and ready to received emails
+ * 
+ * @param {string} email - Email address to test
+ * @param {Options} [options] - Options
+ * @returns {Promise<Boolean>} - Success
+ */
+module.exports.check = (email, options) => {
+	return new Promise((resolve, reject) => { //eslint-disable-line
+		const args = [];
+
+		if (email)
+			args.push(email);
+
+		if (options)
+			args.push(options);
+
+		args.push((success, message) => {
+			success ? resolve(true) : reject(message);
+		});
+
+		checkEmail(...args);
+	});
+};
